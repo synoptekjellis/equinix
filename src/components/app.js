f3ba1f05c6bb11a88f45c040c755fc270c3662fa
the-simian <harlinjesse@gmail.com>
2017-11-20 20:12:40 -0600

    more layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div className="app">XXXXX</div>;
+    return (
+      <div className="app">
+        <div className="world-map" />
+        <div className="data-readout" />
+      </div>
+    );
   }
 }
 
