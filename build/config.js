31733b12f32bf13e6018f06d880cf784a245f712
the-simian <harlinjesse@gmail.com>
2017-12-07 19:05:57 -0600

    updated the config

--- build/config.js
+++ build/config.js
@@ -1,35 +1,43 @@
 function config() {
   window.config = {
-    //Timeframe in hours for ametrics call
+    /* API */
+
+    // Timeframe in hours for a metrics call
     hours: 2,
-    //your bearer token for thousand Eyes
+    // your bearer token for thousand Eyes
     token: '29bc9c84-0bac-4e39-80f3-79d896073113',
-    //Items if this type shall be ignored. Leave as empty array if nothing to blacklist.
+    // Items if this type shall be ignored. Leave as empty array if nothing to blacklist.
     blacklist: ['Oracle Cloud Locations'],
+
+    // The URL of a CORS Proxy require to communicate with Thousand Eyes.
+    // Empty string '' means no proxy
+    corsProxy: 'https://cors-anywhere.herokuapp.com/',
 
     /* MAP COSMETICS */
 
-    //width of lines between countries.
+    // width of lines between countries.
     countryStrokeWidth: 0.25,
-    //base speed for how fast you zoom into a location
+
+    // base speed for how fast you zoom into a location
     zoomTransitionSpeed: 750,
 
-    //Never zoom in farther than this!
+    // Never zoom in farther than this!
     maxZoomLevel: 25,
 
-    //these are the lines connencting nodes
+    // these are the lines connencting nodes
     lineStrokeWidth: 0.25,
 
-    //this is when there is a test selected and you embolden the line between nodes
+    // this is when there is a test selected and you embolden the line between nodes
     lineStrokeWidthActive: 1.75,
 
-    //the unhovered, base radius of an agent
+    // the unhovered, base radius of an agent
     agentMarkerRadius: 3.5,
 
-    //same thing but for tests.
+    // same thing but for tests.
     testMarkerRadius: 2,
 
-    /*Menu cosmetics */
+    /*MENU COSMETICS */
+
     // you may choose from:
     // red orange yellow olive green teal blue violet purple pink brown grey black
     // default is 'equinix red'
