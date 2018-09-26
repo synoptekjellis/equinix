1f49a3da9fb99f28e5e68e66ecac8549f7b2b10b
synoptekjellis <34486972+synoptekjellis@users.noreply.github.com>
2018-09-26 09:26:52 -0700

    u

--- src/state/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import map from './map';
-
-const app = combineReducers({
-  map
-});
-
-export default app;
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-
-import equinixViz from './state/reducers';
-
-let store = createStore(equinixViz, applyMiddleware());
-const { dispatch, getState, subscribe } = store;
-
-export default store;
-export { dispatch, getState, subscribe };
